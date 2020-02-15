58472eafd9f47a073e3eee18f317bec05a99ddd2
Namhyeon, Go <gnh1201@gmail.com>
2020-02-16 05:02:28 +0900

    Create test.php

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+function vbs_tokenize() {
+    $exps = array();
+
+    $symbols = array(
+        '^', '-', '*', '/', '\\', '+', '&',
+        '>', '<', , '=', '(', ')', ',', '%', ' '
+    );
+    $chars = str_split($str);
+    $exp = '';
+    foreach($chars as $char) {
+        if(in_array($char, $symbols)) {
+            $exp = trim($exp);
+            if($exp != '') {
+                $exps[] = trim($exp);
+            }
+            $exp = '';
+            $exps[] = $char;
+        } else {
+            $exp .= $char;
+        }
+    }
+
+    return $exps;
+}
