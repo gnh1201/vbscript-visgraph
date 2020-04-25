a93ae69e2bf57f3c03c77ba8009e2fbb6dd3eb8c
Namhyeon, Go <gnh1201@gmail.com>
2020-04-26 03:08:57 +0900

    Update visgraph.php

--- visgraph.php
+++ visgraph.php
@@ -1,10 +1,8 @@
 <?php
 $contents = file_get_contents("JVC_85190.vbs");
-$lines = explode(PHP_EOL, $contents);
+$lines = explode("\n", $contents);
 
-$relations = array();
-$attributes = array();
-$_SR = array(
+$stop_words = array(
     "on", "error", "resume", "next", "dim",
     "function", "then", "to", "len", "else",
     "if", "end", "next", "replace", "and",
@@ -13,94 +11,115 @@ $_SR = array(
     "sqr", "for", "step", "wscript", "sleep",
     "int", "log", "cos", "asc"
 );
-$_SL = array("dim", "rem", "for");
+
+$stop_opcodes = array("rem");
+$stop_chars = array(" ", "=", "(", ")", "*", "/", "-", "+", ",", "<", ">", ".", "\r");
+
+// (y, b, w, f) = (line, block, expression, is_function)
+$evals = array("executeglobal");
+$blocks = array("main");
+$m = array(
+	array(0, 0, "main", 1)
+);
+
+$y = 0;
+$b = 0;
+$w = "";
+$f = 0;
 
 foreach($lines as $line) {
+	// cleaning the line
+	$doublequote = strpos($line, "\"");
+	if($doublequote !== false) {
+		$line = substr($line, 0, $doublequote);
+	}
+	$line = strtolower($line);
+
+	// increase index of line 
+	$y++;
+
+	// skip when comment
     if(substr($line, 0, 1) == "'") {
         continue;
     }
 
-    $_L = "";
-    $_R = "";
-    $_LR = explode("=", $line);
-    if(count($_LR) > 1) {
-        $_L = $_LR[0];
-        $_R = implode("", array_slice($_LR, 1));
-    } else {
-        $_L = "";
-        $_R = $line;
+	// get tokenized expressions
+	$words = array_filter(explode(" ", str_replace($stop_chars, " ", $line)));
+
+	// get opcode
+	$opcode = current($words);
+
+	// skip when comment
+    if(in_array($opcode, $stop_opcodes)) {
+        continue;
     }
 
-    $_L = trim($_L);
-    $_R = trim($_R);
+	// when function
+	if($opcode == "function") {
+		$f++;
+		$b++;
+		$blocks[$b] = $words[1];
+	}
+	
+	// when end of function
+	if($opcode == "end" && in_array("function", $words)) {
+		$f--;
+	}
 
-    $quotes_pos = strpos($_R, '"');
-    if($quotes_pos !== false) {
-        $_R = trim(substr($_R, 0, $quotes_pos));
-    }
-    
-    if(empty($_L)) {
-        $_L = "VAR";
-    }
-
-    if(empty($_R)) {
-        $_R = "VAR";
-    }
-
-    $_LW = array_filter(explode(" ", trim(str_replace(array("(", ")", "*", "/", "-", "+", ",", "<", ">", "."), " ", $_L))));
-    $_RW = array_filter(explode(" ", trim(str_replace(array("(", ")", "*", "/", "-", "+", ",", "<", ">", "."), " ", $_R))));
-
-    if(current($_RW) == "function") {
-        $_W = next($_RW);
-        $_FN = $_W;
-        //$relations[] = array("FUNCTION", $_FN);
-        $attributes[] = array($_FN, "fontcolor=white,color=red");
-
-        while($_W !== false) {
-            $_W = next($_RW);
-            if(!empty($_W)) {
-                //$relations[] = array("PARAMETER", $_W);
-                $relations[] = array($_FN, $_W);
-                $attributes[] = array($_W, "fontcolor=white,color=blue");
-            }
-        }
-    }
-
-    foreach($_LW as $lw) {
-        foreach($_RW as $rw) {
-            if(!ctype_digit($rw) && !in_array(strtolower($rw), $_SR) && !in_array(strtolower($lw), $_SL)) {
-                /*
-                if($lw != "VAR") {
-                    $relations[] = array("VAR", $lw);
-                }
-                */
-
-                if(($lw . $rw) != "VARVAR") {
-                    $relations[] = array($lw, $rw);
-                }
-            }
-        }
-    }
+	// retrive words
+	foreach($words as $word) {
+		if(!in_array($word, $stop_words)) {
+			// skip when digit
+			if(ctype_digit($word)) {
+				continue;
+			}
+			
+			// (y, b, w) = (line, block, expression, is_function)
+			$m[] = array($y, ($f > 0 ? $b : 0), $word, $f);
+		}
+	}
 }
 
-// add function node
-//$attributes[] = array("FUNCTION", "fontcolor=white,color=red");
-//$attributes[] = array("PARAMETER", "fontcolor=white,color=blue");
-//$attributes[] = array("GLOBAL", "fontcolor=white,color=green");
-$attributes[] = array("VAR", "fontcolor=white,color=green");
-
-// make dinetwork
+// ...
 $dinetwork = "";
-foreach($relations as $rel) {
-    $dinetwork .= sprintf(" %s -> %s;", $rel[0], $rel[1]);
+
+// ...
+foreach($blocks as $blockname) {
+	$dinetwork .= sprintf(" %s [%s]", $blockname, "fontcolor=white,color=red");
 }
 
-$attr_index = array();
-foreach($attributes as $attr) {
-    if(!in_array($attr[0], $attr_index)) {
-        $attr_index[] = $attr[0];
-        $dinetwork .= sprintf(" %s [%s]", $attr[0], $attr[1]);
-    }
+// ...
+foreach($evals as $evalname) {
+	$dinetwork .= sprintf(" %s [%s]", $evalname, "fontcolor=white,color=green");
 }
 
+// ...
+$m2 = array();
+
+// ...
+foreach($m as $r) {
+	if(!array_key_exists($r[2], $m2)) {
+		$m2[$r[2]] = array();
+	}
+
+	if($r[3] == 0 && in_array($r[2], $blocks)) {
+		$m2[$r[2]][] = 0;
+	}
+
+	$m2[$r[2]][] = $r[0];
+}
+
+// 
+
+// ...
+foreach($m2 as $k2=>$r2) {
+	foreach($m2 as $_k2=>$_r2) {
+		$a0 = array_intersect($r2, $_r2);
+		if(count($a0) > 0 && $k2 != $_k2) {
+			$dinetwork .= " {$k2} -> {$_k2};";
+		}
+	}
+}
+
+// ...
 echo sprintf("dinetwork {node[shape=circle]; %s }", $dinetwork);
